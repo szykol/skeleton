31c0f0695b987a73c7e238670d71eec892f9f649
szykol <32578428+KSzymonK@users.noreply.github.com>
2018-08-06 21:36:21 +0200

    - clean up Source.cpp - first commit after removing mess caused by me having other older github accounts on either of my pcs. Those accounts are now removed.

--- SimpleENgine/src/Source.cpp
+++ SimpleENgine/src/Source.cpp
@@ -33,33 +33,6 @@ int main()
     sf::Clock timer;
     sen::FPSCounter counter;
 
-    sen::Button button("STANDARD");
-	button.setSize(button.getSize() * 1.25f);
-	button.addListener(
-		sen::ButtonEvent::STANDARD,
-		[](sen::Button& button) {
-			button.setFillColor({ 0,255,120,50 });
-		},
-		false
-	);
-	button.addListener(
-		sen::ButtonEvent::HOVER,
-		[](sen::Button& button) {
-		button.getTextObject().setString("HOVER");
-		button.setFillColor({ 255,0,120,50 });
-	},
-		true
-	);
-	button.addListener(
-		sen::ButtonEvent::CLICK,
-		[](sen::Button& button) {
-		button.getTextObject().setString("CLICK");
-		button.setFillColor({ 125,0,120,50 });
-	},
-		true
-	);
-    button.setPosition(centerPos);
-
     while (window.isOpen())
     {
         sf::Event evnt;
@@ -89,9 +62,7 @@ int main()
         button.update(window);
         button.render(window);
 
-        //sen::StateManager::run(window);
-
-
+        sen::StateManager::run(window);
 
         window.display();
     }
